e07c35b05f4409cf2c9e26ba2c6d2107856695fb
Isormour <Kucharek15@tlen.pl>
2017-04-23 10:49:42 +0200

    colorpicker

---
+++ Assets/LeaderboardManager.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class LeaderboardManager : MonoBehaviour {
+    public Vector3 InitialPosition;
+    public Vector3 TargetPosition;
+    public MeshRenderer[] Kafelki;
+    public Text[] Texts;
+    // Use this for initializatio
+    bool move;
+    void Start () {
+        this.transform.position = InitialPosition;
+        move = false;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (move)
+        {
+            this.transform.position = Vector3.Lerp(this.transform.position, TargetPosition, 0.1f);
+        }
+    }
+    public void ShowLeaderBoards()
+    {
+        move = true;
+        List<PlayerCharacter> Chars =   GameplayManager.Sceneton.GetCharacters();
+        Dictionary<PlayerCharacter, int> Score = GameplayManager.Sceneton.GetScore();
+        
+
+        for (int i = 0; i < Chars.Count; i++)
+        {
+            Texts[i].text = Score[Chars[i]].ToString();
+        }
+    }
+}
---
+++ Assets/SCRIPTS/ColorPicker.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColorPicker : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+      PlayerController[] Players =  GameManager.Singleton.GetPlayerControllers();
+        for (int i = 0; i < Players.Length; i++)
+        {
+            DSPad.DSPadBehaviour ColorBehav = new DSPad.DSPadBehaviour();
+            ColorBehav.
+            Players[i].PushBehaviour(ColorBehav);
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
